3e224e71829ddc40d6bf163a0ebe3acac1df1a38
Toni <toniponsp@gmail.com>
2015-03-23 20:10:48 +0100

    main

--- app/src/main/java/com/whatatour/whatatour/MainActivity.java
+++ app/src/main/java/com/whatatour/whatatour/MainActivity.java
@@ -1,25 +1,24 @@
-package com.whatatour.prova;
+package com.whatatour.whatatour;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.whatatour.whatatour.R;
 
-public class Prova extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_prova);
+        setContentView(R.layout.activity_main);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_prova, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
